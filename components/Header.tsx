bb194e5dd1cdedc527de517a297228043e5c670d
Djefrid <djeffkuate@gmail.com>
2026-02-02 21:44:24 -0500

    update infomatons

--- components/Header.tsx
+++ components/Header.tsx
@@ -47,7 +47,7 @@ export default function Header() {
             href="#hero"
             className="text-xl font-bold text-white hover:text-primary-400 transition-colors"
           >
-            Portfolio
+            Portfolio Djefrid
           </Link>
 
           {/* Desktop Navigation */}
